f69575aa84e618a724237da6dfdc1b79bd644464
Nicola Cimmino <nicola.cimmino@gmail.com>
2022-11-28 16:33:34 +0100

    Scripts tweaks

--- assembler/build.cmd
+++ assembler/build.cmd
@@ -1,7 +1,8 @@
 @echo off
 if not exist ".build" mkdir ".build"
-copy flash14500.cmd .build
+copy "flash14500.cmd" ".build"
 if not exist ".build\examples" mkdir ".build\examples"
 xcopy "examples" ".build\examples" /Y
-
+copy "..\bootloader\bootloader.ino" ".build"
+copy "..\README.md" ".build"
 dart compile exe "bin\assembler.dart" -o ".build\asm14500.exe"
--- assembler/flash14500.cmd
+++ assembler/flash14500.cmd
@@ -6,3 +6,4 @@ echo flashing %file% to PLC14500 on %port%
 mode %port% dtr=off rts=off baud=9600 parity=n data=8 stop=1
 copy %file% %port% /B
 echo done.
+echo Press RST on the board!
