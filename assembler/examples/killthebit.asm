cc03d046af565949932e14df7ae62130c79b6590
Nicola Cimmino <nicola.cimmino@gmail.com>
2023-02-17 20:25:38 +0100

    Modify Killthebit so it can work even without the RR register mapped to SPR0

--- assembler/examples/killthebit.asm
+++ assembler/examples/killthebit.asm
@@ -1,46 +1,53 @@
 ;
-; Kill the bit for PLC14500-Nano (REV.C or later).
+; Kill the bit for PLC14500-Nano (with slower clock).
 ;
-; NOTE! This game requires REV.C board (or a modified REV.B). This is because the clock
-;   needs to be slowed down for the game to humanly playable. Also revisions prior to C
-;   didn't have the RR wired up to SPR0 so the init code wouldn't work correctly.
+; NOTE! This game requires a REV.B or later board with R30 replaced with a 10K. This is
+;   because the clock needs to be slowed down for the game to humanly playable. If you
+;   feel inclined, you could replace R30 with a 10K trimmer instead, so you can tune the
+;   game speed.
 ;
 ; The game idea itself is from a game for the Altair by Dean McDaniel in 1975.
+; Original Description:
+;   Object: Kill the rotating bit. If you miss the lit bit, another
+;   bit turns on leaving two bits to destroy. Quickly
+;   toggle the switch, don't leave the switch in the up
+;   position. Before starting, make sure all the switches
+;   are in the down position.
 ;
 ; The idea to implement it on the MC14500 came to me after watching this awesome
-;   built by Usagi Electric ( https://www.youtube.com/watch?v=md_cPxVDqeM )
-;   Also the code was heavily adapted from his project:
+;   build by Usagi Electric: https://www.youtube.com/watch?v=md_cPxVDqeM
+;   Also the code was heavily adapted from this one:
 ;   https://github.com/veremenko-y/mc14500-programs/blob/main/sbc1/killthebit.s
 ;
 
 .board=PLC14500-Nano
 
 .io_SWAP=SPR7       ; Swap bit used for temporary storage
+.io_INIT=SPR6       ; Bit indicating initialization has been done
 .io_GAME_BIT0=SPR1  ; Game bit 0
 .io_GAME_BIT1=SPR2  ; Game bit 1
 .io_GAME_BIT2=SPR3  ; Game bit 2
 .io_GAME_BIT3=SPR4  ; Game bit 3
+.io_GAME_BIT4=SPR5  ; Game bit 4
 .io_GAME_LED0=OUT0  ; Game LED 0
 .io_GAME_LED1=OUT1  ; Game LED 1
 .io_GAME_LED2=OUT2  ; Game LED 2
 .io_GAME_LED3=OUT3  ; Game LED 3
-
+.io_GAME_LED4=OUT4  ; Game LED 4
 .io_GAME_BUTTON=IN0 ; Game only button
 
-STO  SWAP        ; Store RR
-ORC  RR          ; Force RR=1
-IEN  RR          ; Enable inputs
-LD   SWAP        ; Restore RR
+IEN  IN6
+OEN  IN6
 
-; This block will be run once at startup
-LDC  RR          ; RR is 0 at reset, but will be 1
-                 ;   once we looped once (see last line)
-OEN  RR          ; Execute below only on the first round.
-STO  GAME_BIT0   ; Initialize game with one bit.
-
-; Here the main loop starts, the below code is executed always.
-ORC  RR          ; 1 -> RR
-OEN  SPR0        ; force enable main loop
+; Initialize the game with one bit.
+; The second time around INIT will be 1 and this block
+; won't execute.
+LDC  INIT
+SKZ
+STO  GAME_BIT0
+LDC  INIT
+SKZ
+STO  INIT
 
 ; Kill (or set!) the first bit.
 ; The bit will be killed if the button is pressed while it's high.
@@ -59,10 +66,14 @@ LD   GAME_BIT2
 STO  GAME_LED2
 LD   GAME_BIT3
 STO  GAME_LED3
+LD   GAME_BIT4
+STO  GAME_LED4
 
 ; Rotate all bits forward (and last back to first).
-LD   GAME_BIT3
+LD   GAME_BIT4
 STO  SWAP
+LD   GAME_BIT3
+STO  GAME_BIT4
 LD   GAME_BIT2
 STO  GAME_BIT3
 LD   GAME_BIT1
@@ -72,12 +83,6 @@ STO  GAME_BIT1
 LD   SWAP
 STO  GAME_BIT0
 
-; Force RR to 1 so when we loop around
-;   the init block will be skipped.
-ORC  RR
-
 ; Don't JMP 0 here, the rest of the code will
 ;   filled with NOPF and act as a delay, otherwise the
 ;   game will be too slow even on the slow clock.
-
-
