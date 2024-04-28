c66e63001389fc8c998d1ce105281966f7f88dee
Nicola Cimmino <nicola.cimmino@gmail.com>
2024-04-28 18:29:43 +0200

    Fix flash command to work with COM above 9

--- bootloader/flash14500.cmd
+++ bootloader/flash14500.cmd
@@ -4,7 +4,7 @@ set port=%2%
 
 echo flashing %file% to PLC14500 on %port%
 mode %port% dtr=off rts=off baud=9600 parity=n data=8 stop=1
-copy %file% %port% /B
+copy %file% \\.\%port% /B
 timeout /t 5
 echo done.
 echo Press RST on the board!
