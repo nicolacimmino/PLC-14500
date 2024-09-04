9e75aeab205e7265f432a6edb1a3c82799c63bd5
Nicola Cimmino <nicolacimmino@users.noreply.github.com>
2024-09-04 17:44:15 +0200

    Fix/rst line (#20)
    * Clear OUT and SPR on program
    
    * Fix RST line pulsing on program upload doesn't work
    
    * Bump bootloader version to 1.3

--- 14500mon/src/bootloader.cpp
+++ 14500mon/src/bootloader.cpp
@@ -1,6 +1,63 @@
 #include "bootloader.h"
 
 /**********************************************************************
+ * Load and run a program that will reset the output latches.
+ * Since we ran out of lines that can be used as outputs there was no
+ *  convenient way to generate a RST pulse after every new program
+ *  load. We work around this by loading in PLC14500 memory a short
+ *  program that will set all output latches and SPR to 0 before loading
+ *  the actual program we want.
+ */
+
+void runResetOutputLatchesProgram()
+{
+  // The binary below is the result of assembling this:
+  //  ORC  RR
+  //  IEN  RR
+  //  OEN  RR
+  //
+  //  LD   IN0
+  //  ANDC IN0
+  //  STO  OUT0
+  //  STO  OUT1
+  //  STO  OUT2
+  //  STO  OUT3
+  //  STO  OUT4
+  //  STO  OUT5
+  //  STO  OUT6
+  //
+  //  STO  SPR0
+  //  STO  SPR1
+  //  STO  SPR2
+  //  STO  SPR3
+  //  STO  SPR4
+  //  STO  SPR5
+  //  STO  SPR6
+  //  STO  SPR7
+  //  JMP  0
+  byte binary[] = {0x76, 0x7A, 0x7B, 0x81, 0x84, 0x88, 0x98, 0xA8, 0xB8, 0xC8, 0xD8, 0xE8,
+                   0x08, 0x18, 0x28, 0x38, 0x48, 0x58, 0x68, 0x78, 0x0C, 0x00};
+
+  acquireBusForWrite();
+
+  int address = 0;
+  while (binary[address] != 0)
+  {
+    writeProgramByte(address, binary[address++]);
+  }
+
+  while (address < 0x100)
+  {
+    writeProgramByte(address++, 0x0F); // NOPF
+  }
+
+  releaseBus();
+
+  // Let it run for a moment so we are sure it had a chance to set outputs.
+  delay(500);
+}
+
+/**********************************************************************
  * Bootstrap the PLC14500 program memory writing the last received
  * program stored in EEPROM.
  */
@@ -15,6 +72,8 @@ void loadBlockIntoProgramMemory(byte block)
   }
 
   Serial.print(F("LOADING"));
+
+  runResetOutputLatchesProgram();
 
   acquireBusForWrite();
 
@@ -31,7 +90,7 @@ void loadBlockIntoProgramMemory(byte block)
   }
 
   releaseBus();
-  
+
   Serial.println(F(""));
 }
 
@@ -107,7 +166,7 @@ uint8_t enterBootloader()
 
       Serial.print("BOOTLOADER TIMEOUT. RX BYYES: ");
       Serial.println(address);
-      
+
       return BOOT_TIMEOUT;
     }
 
